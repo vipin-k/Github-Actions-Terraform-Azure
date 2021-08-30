e4510ce43185b0c86d026c820454c61c4ef4c2c3
vipin-k <vipinkumargupta@gmail.com>
2021-08-30 09:32:34 +0530

    1.2

--- terraform/modules/StorageAccount/main.tf
+++ terraform/modules/StorageAccount/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_storage_account" "STA" {
   name                     = var.sname
-  resource_group_name      = var.rg_name
+  resource_group_name      = var.rgname
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
--- terraform/modules/StorageAccount/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "storageaccount_name" {
-  value = {
-    sta_name = azurerm_storage_account.STA.name #A
-  }
-}
