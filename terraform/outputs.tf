b875b07b6e1916ce02cde3251be96ab4ae47bc0f
vipin-k <vipinkumargupta@gmail.com>
2021-08-30 09:34:38 +0530

    1.2

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,9 +3,3 @@ output "rg_name" {
     appname = module.RG.resourcegroup_name.rg_name
   }
 }
-output "Storage_Account_name" {
-  value = {
-    storageaccount = module.StorageAccount.storageaccount_name.sta_name
-  }
-}
-
