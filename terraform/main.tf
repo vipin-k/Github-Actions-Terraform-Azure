ba2ec3b46d4d221209ef4a71e7973822da171ea9
Dennis Delamida <dennis.delamida@accenture.com>
2023-01-18 15:03:02 +0000

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,3 +17,36 @@ module "SA" {
   rgname   = var.rgname
   location = var.location
 }
+
+
+resource "azurerm_mssql_server" "mtc-sqlserver" {
+  name                         = "mssqlserver01122023"
+  resource_group_name          = var.rgname
+  location                     = var.location
+  version                      = "12.0"
+  administrator_login          = "adminadmin"
+  administrator_login_password = "Password123456"
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_mssql_database" "RAEdb" {
+  name           = "RAEdb"
+  server_id      = azurerm_mssql_server.mtc-sqlserver.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  
+}
+
+resource "azurerm_mssql_database" "ETRAEdb" {
+  name           = "ETRAEdb"
+  server_id      = azurerm_mssql_server.mtc-sqlserver.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+
+  
+ledger_enabled = true
+  
+}
