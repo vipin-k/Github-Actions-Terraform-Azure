3286f90c15aa5bbe2afec5475dcbcc24609034c4
Dennis Delamida <dennis.delamida@accenture.com>
2023-01-18 10:14:00 +0000

    vsdfwf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "TerraformDemo"
-    storage_account_name = "terraformcodeops"
+    resource_group_name  = "terraform"
+    storage_account_name = "terraformcodeops11"
     container_name       = "tfstatefile"
     key                  = "dev.terraform.tfstate"
   }
