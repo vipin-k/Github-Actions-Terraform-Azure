3e2f303faaeb6f8c5e645ff1cc23de9f01d8eab5
Dennis Delamida <dennis.delamida@accenture.com>
2023-01-18 15:18:44 +0000

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,22 +31,4 @@ resource "azurerm_mssql_server" "mtc-sqlserver" {
     environment = "dev"
   }
 }
-
-resource "azurerm_mssql_database" "RAEdb" {
-  name           = "RAEdb"
-  server_id      = azurerm_mssql_server.mtc-sqlserver.id
-  collation      = "SQL_Latin1_General_CP1_CI_AS"
-  license_type   = "LicenseIncluded"
-  
-}
-
-resource "azurerm_mssql_database" "ETRAEdb" {
-  name           = "ETRAEdb"
-  server_id      = azurerm_mssql_server.mtc-sqlserver.id
-  collation      = "SQL_Latin1_General_CP1_CI_AS"
-  license_type   = "LicenseIncluded"
-
-  
-ledger_enabled = true
-  
-}
+ 
