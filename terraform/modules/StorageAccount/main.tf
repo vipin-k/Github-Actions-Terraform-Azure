82d7db2f72fe79b8f16eb692742c6560125594eb
Dennis Delamida <dennis.delamida@accenture.com>
2023-01-19 02:12:26 +0000

    ss

--- terraform/modules/StorageAccount/main.tf
+++ terraform/modules/StorageAccount/main.tf
@@ -9,3 +9,18 @@ resource "azurerm_storage_account" "STA" {
     environment = "dev"
   }
 }
+
+
+resource "azurerm_mssql_server" "mtc-sqlserver" {
+  name                         = "mssqlserver01122023"
+  resource_group_name          = var.rgname
+  location                     = var.location
+  version                      = "12.0"
+  administrator_login          = "adminadmin"
+  administrator_login_password = "Password123456"
+
+  tags = {
+    environment = "dev"
+  }
+}
+ 
