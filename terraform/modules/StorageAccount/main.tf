599d71978a66327cb03f314df8d8d66d9de8f87b
Dennis Delamida <dennis.delamida@accenture.com>
2023-01-19 16:13:00 +0000

    test

--- terraform/modules/StorageAccount/main.tf
+++ terraform/modules/StorageAccount/main.tf
@@ -11,7 +11,7 @@ resource "azurerm_storage_account" "STA" {
 }
 
 resource "azurerm_mssql_server" "mtc-sqlserver" {
-  name                         = "azurermMssqlserver01192023"
+  name                         ="mssqlserver01122023"
   resource_group_name          = var.rgname
   location                     = var.location
   version                      = "12.0"
