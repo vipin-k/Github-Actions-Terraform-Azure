b544160b2664a2a40cd269e73baaefd46c944bb0
Dennis Delamida <dennis.delamida@accenture.com>
2023-01-19 14:35:30 +0000

    test

--- terraform/modules/StorageAccount/main.tf
+++ terraform/modules/StorageAccount/main.tf
@@ -22,11 +22,3 @@ resource "azurerm_mssql_server" "mtc-sqlserver" {
   }
 }
 
-
-resource "azurerm_mssql_database" "RAEdb" {
-  name           = "RAEdb"
-  server_id      = azurerm_mssql_server.mtc-sqlserver.id
-  collation      = "SQL_Latin1_General_CP1_CI_AS"
-  license_type   = "LicenseIncluded"
-  
-}
