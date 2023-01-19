c12c6d804481e67e83098dd7c8d828afef234628
Dennis Delamida <dennis.delamida@accenture.com>
2023-01-19 16:07:27 +0000

    server

--- terraform/modules/StorageAccount/variables.tf
+++ terraform/modules/StorageAccount/variables.tf
@@ -7,10 +7,6 @@ variable "location" {
 variable "rgname" {
   type = string
 }
-variable "dbname" {
-  description = "Azure  dbname"
-  type        = string
-}
 variable "password" {
   description = "Azure  password"
   type        = string
