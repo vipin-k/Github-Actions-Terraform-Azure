0d4fec0f0773abaa5f1cca5f2fa888ff0ec22af6
Dennis Delamida <dennis.delamida@accenture.com>
2023-01-20 09:41:39 +0000

    update rg

--- terraform/modules/StorageAccount/main.tf
+++ terraform/modules/StorageAccount/main.tf
@@ -1,10 +1,14 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.rgname
+  location = var.location
+}
+
 resource "azurerm_storage_account" "STA" {
   name                     = var.sname
-  resource_group_name      = var.rgname
+  resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
-
   tags = {
     environment = "dev"
   }
